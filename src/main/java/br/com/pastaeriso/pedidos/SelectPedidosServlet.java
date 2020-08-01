30cd1e8ed2a2c1d56690f60084f1ca9474235265
Lucas Gueiros <lucasdantasgueiros@gmail.com>
2020-08-01 17:12:40 -0300

    separando o login e url do bd do mybatis

--- src/main/java/br/com/pastaeriso/pedidos/SelectPedidosServlet.java
+++ src/main/java/br/com/pastaeriso/pedidos/SelectPedidosServlet.java
@@ -20,14 +20,22 @@ import br.com.pastaeriso.pedidos.PedidoMapper;
 public class SelectPedidosServlet extends HttpServlet {
 	protected void service (HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-		
+
 		List<Pedido> pedidos;
-  
-		try (SqlSession sqlSession = DatabaseConnection.getInstance().getSqlSessionFactory().openSession()) {
-			PedidoMapper mapper = sqlSession.getMapper(PedidoMapper.class);
+
+		try {
+			PedidoMapper mapper = DatabaseConnection.getInstance()
+															.getSqlSession("falso","falso")
+															.getMapper(PedidoMapper.class);
 			pedidos = mapper.selectPedidos();
+		} catch(Exception e) {
+			request.setAttribute("e",e);
+			RequestDispatcher rd = request.getRequestDispatcher("error.jsp");
+			rd.forward(request,response);
+			return;
 		}
-		
+
+
 		// Separando os pedidos por data de entrega
 		Map<LocalDate,List<Pedido>> map = new HashMap<>();
 		for(Pedido pedido : pedidos) {
@@ -36,7 +44,7 @@ public class SelectPedidosServlet extends HttpServlet {
 			}
 			map.get(pedido.getDataEntrega()).add(pedido);
 		}
-		
+
 		request.setAttribute("map",map);
 		RequestDispatcher rd = request.getRequestDispatcher("pedidos.jsp");
 		rd.forward(request,response);
--- src/main/java/br/com/pastaeriso/servicos/DatabaseConnection.java
+++ src/main/java/br/com/pastaeriso/servicos/DatabaseConnection.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -11,27 +12,44 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class DatabaseConnection {
-	
+
 	private static DatabaseConnection singleton;
-	
+
 	public static DatabaseConnection getInstance(){
 		if(singleton==null) {
 			singleton = new DatabaseConnection();
 		}
 		return singleton;
 	}
-	
-	private SqlSessionFactory sqlSessionFactory = null;
-	
+
+	private SqlSessionFactoryBuilder sqlSessionFactoryBuilder = null;
+	private InputStream inputStream = null;
+
 	private DatabaseConnection(){
 		String resource = "mybatis-config.xml";
 		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 		InputStream inputStream = classloader.getResourceAsStream(resource);
-		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		this.sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
 	}
-	
-	public SqlSessionFactory getSqlSessionFactory(){
-		return sqlSessionFactory;
+
+	// default password
+	public SqlSession getSqlSession(String username, String password){
+		return getSqlSession("jdbc:postgresql://localhost:5432/pastaeriso"
+			,username,password);
 	}
-	
+
+	public SqlSession getSqlSession
+			(String url, String username, String password){
+		Properties properties = new Properties();
+		properties.setProperty("username", username);
+		properties.setProperty("password", password);
+		properties.setProperty("url", url);
+
+		SqlSessionFactory sqlSessionFactory =
+			sqlSessionFactoryBuilder.build(inputStream, properties);
+		try(SqlSession sqlSession = sqlSessionFactory.openSession()){
+			return sqlSession;
+		}
+	}
+
 }
