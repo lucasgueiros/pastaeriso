575088e18ffaa31d34d59ee0f855d370fd6f05a7
Lucas Gueiros <lucasdantasgueiros@gmail.com>
2020-08-04 10:32:19 -0300

    funcionando, mas Enderecos tem algum problema!

--- src/main/java/br/com/pastaeriso/clientes/enderecos/EnderecoMapper.java
+++ src/main/java/br/com/pastaeriso/clientes/enderecos/EnderecoMapper.java
@@ -1,8 +1,11 @@
 package br.com.pastaeriso.clientes.enderecos;
 
+import java.util.List;
+
 public interface EnderecoMapper {
-	
+
 	public Endereco selectEnderecoById(Integer id);
 	public TipoDeEndereco selectTipoDeEnderecoById(Integer id);
-	
+	public List<Endereco> selectEnderecosByClienteId(Integer cliente_id);
+
 }
