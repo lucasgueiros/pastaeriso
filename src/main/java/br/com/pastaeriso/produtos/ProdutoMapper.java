17e03c9ba7f7ae24b5e189992e1cb20f3e6d6dfe
Lucas Gueiros <lucasdantasgueiros@gmail.com>
2020-09-22 14:53:21 -0300

    produto insumo quantidade recuperado

--- src/main/java/br/com/pastaeriso/produtos/ProdutoMapper.java
+++ src/main/java/br/com/pastaeriso/produtos/ProdutoMapper.java
@@ -6,4 +6,5 @@ public interface ProdutoMapper {
   public List<ProdutoPreco> selectProdutoPrecosByProdutoId (Integer id);
   public List<Produto> selectProdutos();
   public Produto selectProdutoPorNome(String nome);
+  public List<ProdutoInsumoQuantidade> selectProdutoInsumoQuantidadeByProdutoId(Integer id);
 }
