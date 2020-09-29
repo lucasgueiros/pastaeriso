c7eb88b88ba91547f163a7759ffc591787d5d4e5
Lucas Gueiros <lucasdantasgueiros@gmail.com>
2020-09-28 22:39:52 -0300

    v1.1.2 - colocando tambem algumas funcoes da MainClass

--- src/main/java/br/com/pastaeriso/MainClass.java
+++ src/main/java/br/com/pastaeriso/MainClass.java
@@ -7,26 +7,40 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.LinkedList;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class MainClass {
   public static void main (String [] args) {
     List<LocalDate> datas = new LinkedList<>();
-    datas.add(LocalDate.of(2020,9,20));
-    datas.add(LocalDate.of(2020,9,19));
-    datas.add(LocalDate.of(2020,9,18));
-    datas.add(LocalDate.of(2020,9,17));
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite a data inicial: ");
+    String inics = scanner.next();
+    System.out.println("Digite a data final: ");
+    String finis = scanner.next();
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    LocalDate inic = LocalDate.parse(inics, formatter);
+    LocalDate fini = LocalDate.parse(finis, formatter);
+
     BigDecimal total = new BigDecimal(0);
-    for(LocalDate data : datas) {
+    String r = "";
+    while(inic.compareTo(fini) <= 0) {
       List<Pedido> pedidos;
+      BigDecimal subtotal = new BigDecimal(0);
       try (SqlSession sqlSession = DatabaseConnection.getInstance().getSqlSessionFactory().openSession()) {
   			PedidoMapper pedidoMapper = sqlSession.getMapper(PedidoMapper.class);
-  			pedidos = pedidoMapper.selectPedidosPorData(data);
+  			pedidos = pedidoMapper.selectPedidosPorData(inic);
   		}
       for(Pedido pedido : pedidos) {
-        total = total.add(pedido.getTotal());
+        subtotal = subtotal.add(pedido.getTotal());
       }
+      r += "Subtotal " + inic + " : " + subtotal +"\n";
+      total = total.add(subtotal);
+      inic = inic.plusDays(1);
     }
-    System.out.println(total);
+    r += "Total: " + total +"\n";
+    System.out.println(r);
 
   }
 }
